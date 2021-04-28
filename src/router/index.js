4fed75f02e3d05b6e09c14fd11bee5b7d333078b
zhang <a254772985@163.com>
2021-04-28 11:21:18 +0800

    zhang

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import index from '@/views/index/index'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [{
+  path: '/',
+  name: 'index',
+  component: index
+}]
 
 const router = new VueRouter({
   mode: 'history',
